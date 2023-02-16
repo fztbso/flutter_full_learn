a0313ba122b06bb368be4a02162d1414f5ce721f
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-02-16 15:18:00 +0300

    text learn with Veli Bacik

--- lib/101/text_learn_view.dart
+++ lib/101/text_learn_view.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
 class TextLearnView extends StatelessWidget {
-  const TextLearnView({super.key});
+   TextLearnView({super.key,});
+  final String name = "Bekir";
+  final ProjectKeys keys = ProjectKeys();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text("Bekir", style: TextStyle(wordSpacing: 2, letterSpacing: 2),)),
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Welcome $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis, //sonuna 3 nokta koymayı sağlıyor
+            textAlign: TextAlign.right,
+            style: TextStyle(wordSpacing: 2,
+                decoration: TextDecoration.underline,
+                fontStyle: FontStyle.italic,
+                color: Colors.red,
+                letterSpacing: 2, fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          Text(
+            "Welcome $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis, //sonuna 3 nokta koymayı sağlıyor
+            textAlign: TextAlign.right,
+            style: ProjectStyles.welcomeStyle,
+          ),
+          Text(
+            "Welcome $name ${name.length}",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis, //sonuna 3 nokta koymayı sağlıyor
+            textAlign: TextAlign.right,
+            style: Theme.of(context).textTheme.headline5?.copyWith(color: ProjectColors.welcomeColor),
+          ),
+          Text(keys.welcome),
+        ],
+      )),
     );
   }
-  
+}
+
+class ProjectStyles {
+  static TextStyle welcomeStyle = TextStyle(wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      color: Colors.red,
+      letterSpacing: 2, fontSize: 16, fontWeight: FontWeight.w600);
+}
+
+class ProjectColors {
+  static Color welcomeColor = Colors.yellow;
+}
+
+class ProjectKeys {
+  final String welcome = "Merhaba";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const TextLearnView(),
+      home: TextLearnView(),
     );
   }
 }
