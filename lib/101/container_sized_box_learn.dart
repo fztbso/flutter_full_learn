e9a35d252866e71f3f33af7913476dac0aab231c
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-02-17 11:40:42 +0300

    container and sized box learn with Veli Bacik

--- lib/101/container_sized_box_learn.dart
+++ lib/101/container_sized_box_learn.dart
@@ -29,14 +29,31 @@ class ContainerSizedBoxLearn extends StatelessWidget {
             child: Text("aa"*100),
             padding: EdgeInsets.all(5),
             margin: EdgeInsets.all(7),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              // shape: BoxShape.circle,
-              border: Border.all(width: 10, color: Colors.white),
-            ),
+            decoration: ProjectContainerDecoration(),
           ),
         ],
       ),
     );
   }
+}
+
+class ProjectUtility {
+  static BoxDecoration boxDecoration = BoxDecoration(
+    //color: Colors.red,
+    gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
+    borderRadius: BorderRadius.circular(15),
+    boxShadow: [BoxShadow(color: Colors.lightGreenAccent, offset: Offset(0.1, 1), blurRadius: 12)],
+    // shape: BoxShape.circle,
+    border: Border.all(width: 10, color: Colors.white),
+  );
+}
+
+class ProjectContainerDecoration extends BoxDecoration {
+  ProjectContainerDecoration() : super(
+    //color: Colors.red,
+    gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
+    borderRadius: BorderRadius.circular(15),
+    boxShadow: [BoxShadow(color: Colors.lightGreenAccent, offset: Offset(0.1, 1), blurRadius: 12)],
+    // shape: BoxShape.circle,
+    border: Border.all(width: 10, color: Colors.white),);
 }
