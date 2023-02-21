3d5045607eb6c43b3136985bf85bd893d3956742
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-02-21 11:01:00 +0300

    column and row learn with Veli Bacik

---
+++ lib/101/column_row_learn.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColumnRowLearn extends StatelessWidget {
+  const ColumnRowLearn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
