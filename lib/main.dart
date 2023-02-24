42cc066b45ac26c2104a16d0dd3e8683c29ca3b2
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-02-24 15:28:12 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,8 @@ import '101/statefull_life_cycle_learn.dart';
 import '101/stateless_learn.dart';
 import '101/text_field_learn.dart';
 import '202/model_learn_view.dart';
-import '202/service/service_learn_view.dart';
+import '202/service/service_learn_get_view.dart';
+import '202/service/service_post_learn_view.dart';
 import '202/tab_learn.dart';
 import 'demos/color_demos_view.dart';
 import 'demos/color_life_cycle_view.dart';
@@ -54,7 +55,7 @@ class MyApp extends StatelessWidget {
 
         inputDecorationTheme: InputDecorationTheme(
           filled: true,
-          fillColor: Colors.white,
+          fillColor: Colors.grey,
           iconColor: Colors.redAccent,
           border: OutlineInputBorder(),
           labelStyle: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.w600)
@@ -92,6 +93,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
 
   @override
   Widget build(BuildContext context) {
