aa78cc689a69f7753f2bf7b4a24ee6ac7437b2f1
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-03-07 15:49:10 +0300

    custom button learn with Veli Bacik

---
+++ lib/product/widget/button/loading_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingButton extends StatefulWidget {
+  const LoadingButton({Key? key, required this.title, required this.callback,}) : super(key: key);
+  final String title;
+  final Future<void> Function() callback;
+  @override
+  State<LoadingButton> createState() => _LoadingButtonState();
+}
+
+class _LoadingButtonState extends State<LoadingButton> {
+
+  bool _isLoading = false;
+  void _changeLoading(){
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () async {
+      if(_isLoading) return;
+      _changeLoading();
+      await widget.callback.call();
+      _changeLoading();
+    }, child:_isLoading ? CircularProgressIndicator() : Text(widget.title));
+  }
+}
