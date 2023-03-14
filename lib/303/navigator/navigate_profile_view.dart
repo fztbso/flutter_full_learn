8bb0026ab2d36ef759accc8aa3bcbbaa96d270da
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-03-14 10:49:18 +0300

    navigate package with Veli Bacik

---
+++ lib/303/navigator/navigate_profile_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NaigateProfileView extends StatefulWidget {
+  const NaigateProfileView({Key? key}) : super(key: key);
+
+  @override
+  State<NaigateProfileView> createState() => _NaigateProfileViewState();
+}
+
+class _NaigateProfileViewState extends State<NaigateProfileView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
