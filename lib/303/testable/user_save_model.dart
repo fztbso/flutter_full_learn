84cd59e7b0cdf7117d47a6a029241f3c6c8f6b2a
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-03-19 00:34:09 +0300

    user save model

--- lib/303/testable/user_save_model.dart
+++ lib/303/testable/user_save_model.dart
@@ -1,9 +1,27 @@
 
 
+import 'dart:isolate';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSaveViewModel {
-  Future<bool> getReadData(String key, SharedPreferences preferences) async {
+  bool getReadData(String key, IStore preferences) {
+    final response = preferences.getString(key);
+    return response;
+  }
+}
+
+abstract class IStore {
+  bool getString(String key);
+}
+
+class SharedPrefStore extends IStore {
+  final SharedPreferences preferences;
+
+  SharedPrefStore(this.preferences);
+
+  @override
+  bool getString(String key) {
     final response = preferences.getString(key);
     if(response?.isNotEmpty ?? false) {
       return true;
@@ -11,4 +29,12 @@ class UserSaveViewModel {
       return false;
     }
   }
+}
+  
+class MockStore extends IStore {
+  @override
+  bool getString(String key) {
+    return false;
+  }
+  
 }
