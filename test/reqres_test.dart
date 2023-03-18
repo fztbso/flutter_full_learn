c0e397fee769bf9f1cc45a74faf07b2d280af7a4
Bekir Sıtkı ÖZBAYIK <fztbso@gmail.com>
2023-03-19 00:32:48 +0300

    reqres test with Veli Bacik

--- test/reqres_test.dart
+++ test/reqres_test.dart
@@ -3,17 +3,29 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_full_learn/303/reqres_resource/service/reqres_service.dart';
 import 'package:flutter_full_learn/303/reqres_resource/viewModel/reqres_provider.dart';
+import 'package:flutter_full_learn/303/testable/user_save_model.dart';
 import 'package:flutter_full_learn/product/global/resource_context.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 @GenerateMocks([ReqResProvider])
 void main() {
-  setUp(() {});
+  setUp(() {
+    Map<String, Object> values = <String, Object>{'bekir': "kamile"};
+    SharedPreferences.setMockInitialValues(values);
+  });
   test("saveToLocale - Test", () {
     var mockProvider = ReqResProvider(ReqresService(Dio()));
     final resourceContext = ResourceContext();
     final result = mockProvider.saveToLocale(resourceContext, []);
 
-    expect(result, []);
+    // expect(result, []);
+  });
+
+  test("saveToLocale - Test", () async {
+    var userViewModel = UserSaveViewModel();
+    final result = userViewModel.getReadData("bekir",MockStore());
+
+    expect(result, false);
   });
 }
